f8b40293ea42dc7726f371e6769f8055a7449b4f
huertas97 <ahuertasg01@gmail.com>
2020-02-24 11:56:38 +0100

    Implementada como funcion

--- Practica_2_PCA/my_modules/my_module.py
+++ Practica_2_PCA/my_modules/my_module.py
@@ -14,8 +14,26 @@ def draw_vector(v0, v1, ax=None):
                     shrinkA=0, shrinkB=0)
     ax.annotate('', v1, v0, arrowprops=arrowprops)
 
-#You can define new functions
 
-##def my_function(variables):
-##    Your code
-##    return variables
+
+# Función empleada para calcular el numero de componentes necesarios para retener  una
+# cantidad deseada de la varianza en una PCA.
+# 
+# Args: 
+# 	-  pca_variance_ratio (list): varianza explicada por cada componente de los datos iniciales
+# 	-  var_expl_deseada (float): cantidad de la varianza que se desea retener
+# 
+# Returns:
+# 	- n_componentes (int): numero de componentes necesarios para explicar la varianza deseada en una PCA
+# 	- varianza_expl  (float): cantidad de varianza explicada por n_componentes
+
+def num_componentes(pca_variance_ratio, var_expl_deseada = 0.99):
+    n_componentes = sum(np.cumsum(pca_variance_ratio) < var_expl_deseada) +1 
+    
+    if n_componentes == len(pca_variance_ratio):
+        varianza_expl = np.cumsum(pca_variance_ratio)[len(pca_variance_ratio)-1]
+         
+    else:
+        varianza_expl = np.cumsum(pca_variance_ratio)[n_componentes-1]
+    
+    return n_componentes, varianza_expl
